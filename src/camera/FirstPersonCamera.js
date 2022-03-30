f49927e341b7e91c9cec864c3c9804b7ddd8b1f0
alok <alksbd9@gmail.com>
2022-03-30 23:51:27 +0545

    Add translation to FirstPersonCamera

--- src/camera/FirstPersonCamera.js
+++ src/camera/FirstPersonCamera.js
@@ -8,10 +8,12 @@ export default class FirstPersonCamera {
         this.camera = camera
         this.xAngle = 0
         this.yAngle = 0
+        this.velocity = {x: 0, y: 0}
     }
 
     update() {
         this.#updateRotation()
+        this.#updatePosition()
     }
 
     #updateRotation() {
@@ -28,5 +30,32 @@ export default class FirstPersonCamera {
         quaternion.multiply(xQuaternion)
     
         this.camera.quaternion.copy(quaternion)
+    }
+
+    #updatePosition() {
+        let speed = 1
+        if(InputController.keyDown == 'w') this.velocity.y = speed
+        if(InputController.keyDown == 'a') this.velocity.x = speed
+        if(InputController.keyDown == 's') this.velocity.y = -speed
+        if(InputController.keyDown == 'd') this.velocity.x = -speed
+        
+        if(InputController.keyUp == 'w') this.velocity.y = 0
+        if(InputController.keyUp == 'a') this.velocity.x = 0
+        if(InputController.keyUp == 's') this.velocity.y = 0
+        if(InputController.keyUp == 'd') this.velocity.x = 0
+        
+        const yQuaternion = new THREE.Quaternion()
+        yQuaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.yAngle)
+    
+        const forward = new THREE.Vector3(0, 0, -1)
+        forward.applyQuaternion(yQuaternion)
+        forward.multiplyScalar(this.velocity.y)
+    
+        const sideways = new THREE.Vector3(-1, 0, 0)
+        sideways.applyQuaternion(yQuaternion)
+        sideways.multiplyScalar(this.velocity.x)
+    
+        this.camera.position.add(forward)
+        this.camera.position.add(sideways)
     }
 }
